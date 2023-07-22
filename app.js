15bd27883c112d30d97a7747a381ceae300140a0
Onah Patience Tochukwu <65729360+tchytech@users.noreply.github.com>
2023-07-22 20:08:58 +0100

    done;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;

---
+++ app.js
@@ -0,0 +1,8 @@
+const exploreTabs = document.querySelectorAll('.tab-btn');
+const exploreContent = document.querySelectorAll('.explore-cards');
+
+exploreTabs.forEach((tab, index)=>{
+    tab.addEventListener('click', ()=>{
+        tab.classList.add;;;;;;;;;;;;;;
+    })
+})
