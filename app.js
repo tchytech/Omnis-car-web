285702e22c419563ba6ae3e990bdaa485efb42c2
Onah Patience Tochukwu <65729360+tchytech@users.noreply.github.com>
2023-07-22 13:54:35 -0700

    update to files made

--- app.js
+++ app.js
@@ -2,7 +2,16 @@ const exploreTabs = document.querySelectorAll('.tab-btn');
 const exploreContent = document.querySelectorAll('.explore-cards');
 
 exploreTabs.forEach((tab, index)=>{
-    tab.addEventListener('click', ()=>{
-        tab.classList.add;;;;;;;;;;;;;;
+    tab.addEventListener('click',  (e)=>{
+        exploreTabs.forEach(tab=>{tab.classList.remove('active')});
+        tab.classList.add('active');
+
+        let exploreLine = document.querySelector('.explore-line');
+        exploreLine.style.width = e.target.offsetWidth + "px";
+        exploreLine.style.left = e.target.offsetLeft + "px";
+
+        exploreContent.forEach(content=>{content.classList.remove('active')});
+        exploreContent[index].classList.add('active');
     })
+
 })
